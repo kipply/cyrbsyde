f081e71f394f1640e5d6fb2bb84159ced46ccea2
Carol Chen <carolyfisher@gmail.com>
2017-09-09 07:33:14 -0400

    Functioning map

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,13 +1,5 @@
 import React from 'react';
-import {
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import { Image, Platform, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { WebBrowser } from 'expo';
 import { Navigator, NativeModules } from 'react-native';
 import * as firebase from "firebase";
@@ -29,23 +21,23 @@ const uiTheme = {
 // const credentials = firebase.auth.GoogleAuthProvider.credential("194804647786-mqj2c9nqeebcgsqh6100okln19u0hfut.apps.googleusercontent.com", "M_ap9dgzFPnVMIPs1Ufakb5T");
 
 export default class LoginScreen extends React.Component {
-  static navigationOptions = {
-    header: null,
-  };
-  handleClick = () => {
-    //   var provider = new firebase.auth.GoogleAuthProvider();
-    //   firebase.auth().signInWithCredential(credentials)
-    //       .then()
-    //       .catch(function(error) {
-    //   });
-      console.log(firebase.auth().currentUser);
-  }
+    static navigationOptions = {
+        header: null,
+    };
+    handleClick = () => {
+        //   var provider = new firebase.auth.GoogleAuthProvider();
+        //   firebase.auth().signInWithCredential(credentials)
+        //       .then()
+        //       .catch(function(error) {
+        //   });
+        console.log(firebase.auth().currentUser);
+    }
     render() {
         return (
             <View>
-                <ThemeProvider uiTheme={uiTheme}>
-                    <Button onPress={this.handleClick} raised primary text="LOGIN" />
-                </ThemeProvider>
+            <ThemeProvider uiTheme = {uiTheme}>
+            <Button onPress = {this.handleClick} raised primary text = "LOGIN" / >
+            </ThemeProvider>
             </View>
         );
     }
